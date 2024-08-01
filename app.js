fd11e2e26935ffa9377a3b34eec256b8292d4913
Farhat <154868269+Farhat0077@users.noreply.github.com>
2024-08-01 05:47:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port= process.env.PORT || 9876
+const port= process.env.PORT || 3000
 
 
 const start=async()=>{
